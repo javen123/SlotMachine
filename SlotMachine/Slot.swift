d93d0294248bd54ccd6bfc12ef21c7bfc88b8f83
javen123 <thisjave@gmail.com>
2014-09-30 10:18:47 -0500

    First Commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,18 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by user on 9/29/14.
+//  Copyright (c) 2014 Neva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    
+    var value = 0
+    var image:UIImage = UIImage(named:"Ace")
+    var isRed = true
+}
+
